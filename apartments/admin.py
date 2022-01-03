751e4acd3e53b389fd794d2eb252ae92ad81e454
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-03 23:11:07 +0000

    Modify how the data is displayed on the admin

--- apartments/admin.py
+++ apartments/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Apartment
 
-# Register your models here.
+
+@admin.register(Apartment)
+class ApartmentModel(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "for_sale",
+        "for_rent",
+        "bedrooms",
+        "baths",
+    )
+    list_filter = (
+        "for_sale",
+        "for_rent",
+        "bedrooms",
+        "baths",
+        "self_contained",
+        "verified",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "location",
+    )
+    readonly_fields = (
+        "total_rent_payment",
+        "agent_fee",
+    )
+    save_on_top = True
