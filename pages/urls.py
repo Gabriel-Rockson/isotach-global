a8599ef38639e8d345b01ef48d1e931fc1ab52b6
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-03 15:56:59 +0000

    Add pages app and add HomePage view and url conf

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import HomePage
+
+app_name="pages"
+urlpatterns = [
+    path('', HomePage.as_view(), name="homepage"),
+]
---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+
+class HomePage(TemplateView):
+    template_name = "pages/homepage.html"
+    
