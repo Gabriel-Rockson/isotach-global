1f5b25f5a35a90f203192082c57fca2f638048d5
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-04 15:14:38 +0000

    Format code

--- agents/admin.py
+++ agents/admin.py
@@ -5,7 +5,13 @@ from .models import Agent
 
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "image_tag", "email", "phone_number", "verified",)
-    list_filter = ("verified",)
-    search_fields = ("full_name",)
+    list_display = (
+        "full_name",
+        "image_tag",
+        "email",
+        "phone_number",
+        "verified",
+    )
+    list_filter = ("verified", )
+    search_fields = ("full_name", )
     readonly_fields = ['image_tag']
