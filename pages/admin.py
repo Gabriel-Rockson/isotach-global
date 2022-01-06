9770d1e7fafb4d4d5525511182ada61ff839f310
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-06 15:20:49 +0000

    Register the models on the admin interface

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import AboutSection, ServicesSection, Service
 
-# Register your models here.
+
+class ServiceInline(admin.StackedInline):
+    model = Service
+    extra = 1
+    min_num = 1
+    max_num = 4
+
+@admin.register(ServicesSection)
+class ServicesSectionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [
+        ServiceInline,
+    ]
