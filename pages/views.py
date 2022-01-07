e5bcb7fcc5bc32a5359592025f3f2f84c585da07
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-07 21:54:32 +0000

    Add extra context to the HomePage

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from .models import HomePage
+from apartments.models import Apartment
 
 from django.views.generic import TemplateView
 
@@ -6,5 +7,7 @@ from django.views.generic import TemplateView
 class HomePage(TemplateView):
     template_name = "pages/homepage.html"
     extra_context = {
-        "homepage": HomePage.objects.first()
+        "homepage": HomePage.objects.first(),
+        "featured_apartments": Apartment.objects.filter(featured=True)[:8],
+        "latest_apartments": Apartment.objects.all()[:8]
     }
