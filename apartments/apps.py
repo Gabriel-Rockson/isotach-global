62a72d691b554bf67e946417d70e695a8eadd6bc
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-15 19:47:26 +0000

    Add signal to convert the apartment images to webp

--- apartments/apps.py
+++ apartments/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApartmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apartments'
+
+    def ready(self):
+        import apartments.signals
