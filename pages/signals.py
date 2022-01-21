3f21f7d648d27370911de7f138899f40882c9cdc
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-21 03:10:10 +0000

    Add function to reduce the brightness of a picture and use this to reduce the brightness of the banner image

--- pages/signals.py
+++ pages/signals.py
@@ -2,13 +2,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from pages.models import HomePage
-from utils.image_manipulation import resize_image, convert_image_to_webp
+from utils.image_manipulation import resize_image, reduce_brightness
 
 from pathlib import Path
 
 
 @receiver(post_save, sender=HomePage)
-def resize_homepage_banner_image_and_convert_to_webp(sender, instance, **kwargs):
-    resize_image(instance.banner_image, instance.banner_image.name, 1280, 720)
-    # convert_to_webp(instance.banner_image, instance.banner_image.name)
-    convert_image_to_webp(Path(instance.banner_image.path))
+def resize_homepage_banner_image(sender, instance, **kwargs):
+    resize_image(instance.banner_image, instance.banner_image.name, 2400, 1600)
+
+
+@receiver(post_save, sender=HomePage)
+def reduce_brightness_of_homepage_banner_image(sender, instance, **kwargs):
+    reduce_brightness(instance.banner_image, instance.banner_image.name, 0.4)
--- utils/image_manipulation.py
+++ utils/image_manipulation.py
@@ -1,5 +1,5 @@
 from django.core.files.storage import default_storage as storage
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 
 def resize_image(image, image_name, width, height):
@@ -13,6 +13,19 @@ def resize_image(image, image_name, width, height):
         resized_image.save(fh, picture_format)
 
 
+def reduce_brightness(image, image_name, factor):
+    img = Image.open(image)
+    enhancer = ImageEnhance.Brightness(img)
+    new_img = enhancer.enhance(factor)
+
+    with storage.open(image_name, 'wb') as fh:
+        picture_format = 'JPEG'
+        new_img.save(fh, picture_format)
+
+
+# TODO figure a way to solve this issue
+# TODO we need to convert a given image to webp format
+
 # def convert_to_webp(image, image_name):
 #     img = Image.open(image)
     
@@ -21,7 +34,11 @@ def resize_image(image, image_name, width, height):
 #         img.save(fh, picture_format)
 
 
-def convert_image_to_webp(source):
-    destination = source.with_suffix(".webp")
-    image = Image.open(source)
-    image.save(destination, format="webp")
+# # def convert_image_to_webp(source):
+# #     destination = source.with_suffix(".webp")
+# #     image = Image.open(source)
+# #     image.save(destination, format="webp")
+
+# def convert_image_to_webp(source):
+#     image = Image.open(source)
+#     image.save(source, format="webp")
