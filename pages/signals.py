acc3327cd3107f769d060e9d97b88f7f80e14b46
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-21 02:17:06 +0000

    Add function to convert the image to webp format

--- pages/signals.py
+++ pages/signals.py
@@ -2,9 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from pages.models import HomePage
-from utils.image_manipulation import resize_image
+from utils.image_manipulation import resize_image, convert_to_webp, convert_image_to_webp
+
+from pathlib import Path
 
 
 @receiver(post_save, sender=HomePage)
-def resize_homepage_banner_image(sender, instance, **kwargs):
+def resize_homepage_banner_image_and_convert_to_webp(sender, instance, **kwargs):
     resize_image(instance.banner_image, instance.banner_image.name, 1280, 720)
+    # convert_to_webp(instance.banner_image, instance.banner_image.name)
+    convert_image_to_webp(Path(instance.banner_image.path))
--- utils/image_manipulation.py
+++ utils/image_manipulation.py
@@ -5,9 +5,23 @@ from PIL import Image
 def resize_image(image, image_name, width, height):
     size = (width, height)
 
-    image = Image.open(image)
-    resized_image = image.resize(size, Image.ANTIALIAS)
+    img = Image.open(image)
+    resized_image = img.resize(size, Image.ANTIALIAS)
 
     with storage.open(image_name, 'wb') as fh:
         picture_format = 'JPEG'
         resized_image.save(fh, picture_format)
+
+
+# def convert_to_webp(image, image_name):
+#     img = Image.open(image)
+    
+#     with storage.open(image_name, 'wb') as fh:
+#         picture_format = 'webp'
+#         img.save(fh, picture_format)
+
+
+def convert_image_to_webp(source):
+    destination = source.with_suffix(".webp")
+    image = Image.open(source)
+    image.save(destination, format="webp")
