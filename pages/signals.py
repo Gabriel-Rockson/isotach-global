53055514b2afa5efe05d3a2a3a4c6d2ae3f9f6d3
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-22 19:22:57 +0000

    Enable the choosing of image to be used for the banner image by the admin

--- pages/signals.py
+++ pages/signals.py
@@ -1,17 +1,17 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# from pages.models import HomePage
-# from utils.image_manipulation import resize_image, reduce_brightness
+from pages.models import HomePage
+from utils.image_manipulation import resize_image, reduce_brightness, reduce_quality
 
-# from pathlib import Path
-
-
-# @receiver(post_save, sender=HomePage)
-# def resize_homepage_banner_image(sender, instance, **kwargs):
-#     resize_image(instance.banner_image, instance.banner_image.name, 2400, 1600)
+from pathlib import Path
 
 
-# @receiver(post_save, sender=HomePage)
-# def reduce_brightness_of_homepage_banner_image(sender, instance, **kwargs):
-#     reduce_brightness(instance.banner_image, instance.banner_image.name, 0.4)
+@receiver(post_save, sender=HomePage)
+def reduce_quality_homepage_banner_image(sender, instance, **kwargs):
+    reduce_quality(instance.banner_image, instance.banner_image.name, new_width=1920)
+
+
+@receiver(post_save, sender=HomePage)
+def reduce_brightness_of_homepage_banner_image(sender, instance, **kwargs):
+    reduce_brightness(instance.banner_image, instance.banner_image.name, 0.4)
