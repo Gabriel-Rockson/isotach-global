6b69da3c8bb04c5ec0c767418814242a43b6efbe
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-23 03:11:54 +0000

    Modify the styling of the website

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["../templates/**/*.html", "../templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red": {
+          "blood": "#660708",
+          "ruby": "#a4161a",
+          "carnelian": "#ba181b",
+          "imperial": "#e5383b"
+        },
+        "black": {
+          "rich": "#0b090a",
+          "eerie": "#161a1d",
+        },
+        "silverchalice": "#b1a7a6",
+        "lightgray": "#d3d3d3",
+        "cultured": "#f5f3f4",
+        "royal-blue-dark": "#001177",
+        "link-blue": "#0000FF",
+        "link-purple": "#800080",
+      }
+    },
   },
   plugins: [],
 }
