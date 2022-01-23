0cbcd119f050187b0445f68d22f7c8e3b488a2be
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-23 23:28:31 +0000

    Modify template tag to get the image url of the varous page sections

--- pages/templatetags/pages_extras.py
+++ pages/templatetags/pages_extras.py
@@ -3,20 +3,23 @@ from django import template
 
 register = template.Library()
 
+from pages.models import HomePage, AboutSection, ServicesSection
+
 
 @register.simple_tag
 def get_section_featured_image(section):
-    print(section)
-    if section == 'homepage':
+    # print('---------------------------------------------------------------------------------------')
+    # print(section)
+    # print('---------------------------------------------------------------------------------------')
+    if isinstance(section, HomePage):
+        print(section)
         url = Path(section.banner_image.url)
-        print(url)
         return {
             "webp": url.with_suffix('.webp'),
             "jpeg": url.with_suffix('.jpg'),
         }
-    elif section == 'homepage.about_section' or section == 'homepage.services_sectoin':
+    elif isinstance(section, AboutSection) or isinstance(section, ServicesSection):
         url = Path(section.featured_image.url)
-        print(url)
         return {
             "webp": url.with_suffix('.webp'),
             "jpeg": url.with_suffix('.jpg'),
