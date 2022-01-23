63a1296821457fb61bf3c70332fa15d154a9b183
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-23 17:49:28 +0000

    Add signals to create thumbnails and also optimize the about and services section featured images

--- apartments/signals.py
+++ apartments/signals.py
@@ -1,11 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import ApartmentImage
-from utils.image_manipulation import reduce_quality, resize_image
+from .models import ApartmentImage, Apartment
+from utils.image_manipulation import reduce_quality, resize_image, create_thumbnail
 
 
 @receiver(post_save, sender=ApartmentImage)
 def reduce_apartment_image_quality_and_optimize(sender, instance, **kwargs):
     """Take a quality factor between 1 to 100 and optimize the image"""
-    reduce_quality(instance.image, instance.image.name, new_width=1920, quality=70)
+    reduce_quality(instance.image, instance.image.name,
+                   new_width=1920, quality=70)
+
+
+@receiver(post_save, sender=ApartmentImage)
+def create_thumbnail_for_first_image(sender, instance, **kwargs):
+    if instance == instance.apartment.images.first():
+        create_thumbnail(instance.image, instance.image.name,
+                         new_width=400, new_height=400)
--- pages/signals.py
+++ pages/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from pages.models import HomePage
+from pages.models import HomePage, AboutSection, ServicesSection
 from utils.image_manipulation import resize_image, reduce_brightness, reduce_quality
 
 from pathlib import Path
@@ -9,9 +9,22 @@ from pathlib import Path
 
 @receiver(post_save, sender=HomePage)
 def reduce_quality_homepage_banner_image(sender, instance, **kwargs):
-    reduce_quality(instance.banner_image, instance.banner_image.name, new_width=1920)
+    reduce_quality(instance.banner_image,
+                   instance.banner_image.name, new_width=1920)
 
 
 @receiver(post_save, sender=HomePage)
 def reduce_brightness_of_homepage_banner_image(sender, instance, **kwargs):
-    reduce_brightness(instance.banner_image, instance.banner_image.name, 0.4)
+    reduce_brightness(instance.banner_image, instance.banner_image.name, 0.3)
+
+
+@receiver(post_save, sender=AboutSection)
+def reduce_quality_about_section_featured_image(sender, instance, **kwargs):
+    reduce_quality(instance.featured_image,
+                   instance.featured_image.name, new_width=720, quality=65)
+
+
+@receiver(post_save, sender=ServicesSection)
+def reduce_quality_services_section_featured_image(sender, instance, **kwargs):
+    reduce_quality(instance.featured_image,
+                   instance.featured_image.name, new_width=720, quality=65)
