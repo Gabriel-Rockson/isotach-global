54c7d7e29c67e33aa07018a9cfc39544879fdf98
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-23 17:48:02 +0000

    Add utility function to create thumbnails for the first image uploaded for an image

--- utils/image_manipulation.py
+++ utils/image_manipulation.py
@@ -2,11 +2,15 @@ from django.core.files.storage import default_storage as storage
 from PIL import Image, ImageEnhance
 
 
-def resize_image(image, image_name, new_width):
+def resize_image(image, image_name, new_width, new_height=None):
     img = Image.open(image)
 
-    new_height = int((img.height * new_width) / img.width)
-    size = new_width, new_height
+    if new_height is None:
+        new_height = int((img.height * new_width) / img.width)
+        size = new_width, new_height
+    else:
+        size = new_height, new_height
+
     resized_image = img.resize(size, Image.ANTIALIAS)
 
     with storage.open(image_name, 'wb') as fh:
@@ -38,6 +42,30 @@ def reduce_quality(image, image_name, new_width=None, quality=75):
     with storage.open(image_name, 'wb') as fh:
         new_name = fh.name.split('.')[0] + '.webp'
 
-        resized_image.save(fh, format=img.format, quality=quality, optimize=True)
+        resized_image.save(fh, format=img.format,
+                           quality=quality, optimize=True)
         resized_image.save(new_name, format="WEBP",
+                           quality=quality, optimize=True)
+
+
+def create_thumbnail(image, image_name, new_width, new_height=None, quality=75):
+    img = Image.open(image)
+
+    original_image = resize_image(image, image_name, img.width)
+
+    if new_height is None:
+        resized_image = resize_image(image, image_name, new_width)
+    else:
+        resized_image = resize_image(image, image_name, new_width, new_height)
+
+    with storage.open(image.name, 'wb') as fh:
+        webp_thumbnail = fh.name.split('.')[0] + '.thumbnail.webp'
+        jpeg_thumbnail = fh.name.split('.')[0] + '.thumbnail.jpg'
+
+        # save the original 
+        original_image.save(fh, format=img.format)
+
+        resized_image.save(webp_thumbnail, format="WEBP",
+                           quality=quality, optimize=True)
+        resized_image.save(jpeg_thumbnail, format=img.format,
                            quality=quality, optimize=True)
