276f57196daa06f6d2d77706caeb112dfd545336
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-24 19:38:23 +0000

    Enable webp image formats for the homepage

--- pages/templatetags/pages_extras.py
+++ pages/templatetags/pages_extras.py
@@ -9,13 +9,10 @@ from pages.models import HomePage, AboutSection, ServicesSection
 @register.simple_tag
 def get_section_featured_image(section):
     if isinstance(section, HomePage):
-        return {
-            "jpeg": section.banner_image.url,
-        }
+        url = Path(section.banner_image.url)
+        return {"jpeg": url.with_suffix(".jpg"), "webp": url.with_suffix(".webp")}
     elif isinstance(section, AboutSection) or isinstance(section, ServicesSection):
         return {
             "jpeg": section.featured_image.url,
         }
-    return {
-        "jpeg": None
-    }
+    return {"jpeg": "Not found"}
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -8,7 +8,7 @@ from time import time
 
 # resize image
 def resize_image(image_name, width, height=None, quality=85, format="JPEG"):
-    with storage.open(image_name, 'rb') as m:
+    with storage.open(image_name, "rb") as m:
         im = Image.open(m)
 
         if height is None:
@@ -22,13 +22,15 @@ def resize_image(image_name, width, height=None, quality=85, format="JPEG"):
         bfile = BytesIO()
         im.save(bfile, format=format, quality=quality)
 
-    with storage.open(image_name, 'wb') as i:
+    with storage.open(image_name, "wb") as i:
         i.write(bfile.getbuffer())
 
 
-def resize_and_reduce_brightness(image_name, width, height=None, factor=0.4, quality=75):
+def resize_and_reduce_brightness(
+    image_name, width, height=None, factor=0.4, quality=75
+):
 
-    with storage.open(image_name, 'rb') as m:
+    with storage.open(image_name, "rb") as m:
         im = Image.open(m)
         enhancer = ImageEnhance.Brightness(im)
 
@@ -42,15 +44,25 @@ def resize_and_reduce_brightness(image_name, width, height=None, factor=0.4, qua
         im = im.resize(size, Image.ANTIALIAS)  # resize the image
 
         bfile = BytesIO()
-        im.save(bfile, format="JPEG", quality=quality,
-                optimize=True)  # optimize the image
+        im.save(
+            bfile, format="JPEG", quality=quality, optimize=True
+        )  # optimize the image
 
-    with storage.open(image_name, 'wb') as i:
+        bfile_webp = BytesIO()
+        im.save(bfile_webp, format="WEBP", quality=quality)
+
+    with storage.open(image_name, "wb") as i:
         i.write(bfile.getbuffer())
+
+    filepath, ext = image_name.split(".")
+    filename_webp = f"{filepath}.webp"
+    
+    with storage.open(filename_webp, "wb") as i:
+        i.write(bfile_webp.getbuffer())
 
 
 def create_thumbnail(image_name, width, height=None, quality=75, format="JPEG"):
-    with storage.open(image_name, 'rb') as m:
+    with storage.open(image_name, "rb") as m:
         im = Image.open(m)
 
         if height is None:
@@ -67,12 +79,12 @@ def create_thumbnail(image_name, width, height=None, quality=75, format="JPEG"):
         bfile_webp = BytesIO()
         im.save(bfile_webp, format="WEBP", quality=quality)
 
-    filepath, ext = image_name.split('.')
+    filepath, ext = image_name.split(".")
     filename_jpeg = f"{filepath}.thumbnail.{ext}"
     filename_webp = f"{filepath}.thumbnail.webp"
 
-    with storage.open(filename_jpeg, 'wb') as i:
+    with storage.open(filename_jpeg, "wb") as i:
         i.write(bfile_jpeg.getbuffer())
 
-    with storage.open(filename_webp, 'wb') as i:
+    with storage.open(filename_webp, "wb") as i:
         i.write(bfile_webp.getbuffer())
