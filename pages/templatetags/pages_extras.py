99d4be90caf8c45a8ea212745a5765e2c5cd3899
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-28 19:42:49 +0000

    Modify the pages templatetag to just use split to get the various formats of images

--- pages/templatetags/pages_extras.py
+++ pages/templatetags/pages_extras.py
@@ -9,9 +9,17 @@ from pages.models import HomePage, AboutSection
 @register.simple_tag
 def get_section_featured_image(section):
     if isinstance(section, HomePage):
-        url = Path(section.banner_image.url)
-        return {"jpeg": url.with_suffix(".jpg"), "webp": url.with_suffix(".webp")}
+        
+        url = section.banner_image.url
+        parts = url.split(".jpg")
+        webp_url = parts[0] + ".webp"
+        
+        return {"jpeg": url, "webp": webp_url}
     elif isinstance(section, AboutSection):
-        url = Path(section.featured_image.url)
-        return {"jpeg": url.with_suffix(".jpg"), "webp": url.with_suffix(".webp")}
+        
+        url = section.featured_image.url
+        parts = url.split(".jpg")
+        webp_url = parts[0] + ".webp"
+        
+        return {"jpeg": url, "webp": webp_url}
     return {"jpeg": "Not found", "webp": "Not Found"}
