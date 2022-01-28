7b2d8e70d01756682e018b31c4852dd4d395242f
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-28 19:57:08 +0000

    Use the split method to get the thumbnail images

--- apartments/templatetags/apartments_extras.py
+++ apartments/templatetags/apartments_extras.py
@@ -9,10 +9,11 @@ def paginate_url(page_num, page_field_name, urlencode=None):
     url = f"?{page_field_name}={page_num}"
 
     if urlencode:
-        query_string = urlencode.split('&')
-        filtered_query_string = filter(lambda item: item.split('=')[
-                                       0] != page_field_name, query_string)
-        encoded_query_string = '&'.join(filtered_query_string)
+        query_string = urlencode.split("&")
+        filtered_query_string = filter(
+            lambda item: item.split("=")[0] != page_field_name, query_string
+        )
+        encoded_query_string = "&".join(filtered_query_string)
 
         url = f"{url}&{encoded_query_string}"
 
@@ -22,12 +23,11 @@ def paginate_url(page_num, page_field_name, urlencode=None):
 @register.simple_tag
 def get_thumbnail_images(apartment):
     if apartment.images:
-        url = Path(apartment.images.first().image.url)
-        return {
-            "jpeg": url.with_suffix('.thumbnail.jpg'),
-            "webp": url.with_suffix('.thumbnail.webp')
-        }
-    return {
-        "jpeg": "Image Not Found",
-        "webp": "Image Not Found"
-    }
+
+        url = apartment.images.first().image.url
+        parts = url.split(".jpg")
+        jpeg_thumbnail = parts[0] + ".thumbnail.jpg"
+        webp_thumbnail = parts[0] + ".thumbnail.webp"
+
+        return {"jpeg": jpeg_thumbnail, "webp": webp_thumbnail}
+    return {"jpeg": "Image Not Found", "webp": "Image Not Found"}
