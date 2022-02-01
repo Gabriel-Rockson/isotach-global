54a05983cb3dc68b86b5ddfecc39f755dabde7ff
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-01 02:52:59 +0000

    Register the contact form on the admin interface

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactForm
+
+
+@admin.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "city", "phone_number", "email", "date_and_time"]
+    search_fields = ["full_name", "city", "email"]
+    ordering = ("-date_and_time",)
