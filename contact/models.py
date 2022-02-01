d1fb7611874a2ba7877cc5ed0fe0c4f3465a3e2a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-01 02:46:42 +0000

    Add contact form model

--- contact/models.py
+++ contact/models.py
@@ -1,1 +1,40 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from django.utils.translation import gettext_lazy as _
+
+
+class ContactForm(models.Model):
+    full_name = models.CharField(
+        verbose_name=_("Full Name"),
+        max_length=50,
+        help_text="Enter your full name",
+        null=False,
+        blank=False,
+    )
+    city = models.CharField(
+        verbose_name=_("City / Town"),
+        max_length=100,
+        help_text="Enter your city or town",
+        null=False,
+        blank=False,
+    )
+    phone_number = PhoneNumberField()
+    email = models.EmailField(
+        verbose_name=_("Email Address"),
+        help_text="Enter your email address",
+        max_length=300,
+        blank=False,
+        null=False,
+    )
+    message = models.TextField(
+        verbose_name=_("Message"), help_text="Enter your message", max_length=1000
+    )
+    date_and_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = "Contact Message"
+        verbose_name_plural = "Contact Messages"
