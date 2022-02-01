fffddb4d9173d08997f95b6d1952ac3e9adcb683
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-01 19:49:01 +0000

    Make the contact info on the admin interface read only

--- contact/admin.py
+++ contact/admin.py
@@ -8,3 +8,4 @@ class ContactFormAdmin(admin.ModelAdmin):
     list_display = ["full_name", "city", "phone_number", "email", "date_and_time"]
     search_fields = ["full_name", "city", "email"]
     ordering = ("-date_and_time",)
+    readonly_fields = ["full_name", "city", "phone_number", "email", "date_and_time", "message"]
