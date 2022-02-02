3678cb90b213ac476f14611d2b8b3d14a834ae31
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-02 20:00:03 +0000

    Create a base abstract model base to have the information common to the other models

--- contact/models.py
+++ contact/models.py
@@ -4,32 +4,33 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-class Contact(models.Model):
+class BasicContactInformation(models.Model):
     full_name = models.CharField(
         verbose_name=_("Full Name"),
         max_length=50,
-        help_text="Enter your full name",
         null=False,
         blank=False,
     )
-    city = models.CharField(
-        verbose_name=_("City / Town"),
-        max_length=100,
-        help_text="Enter your city or town",
-        null=False,
-        blank=False,
-    )
-    phone_number = PhoneNumberField()
+    phone_number = PhoneNumberField(blank=True, null=True)
     email = models.EmailField(
         verbose_name=_("Email Address"),
-        help_text="Enter your email address",
         max_length=300,
         blank=False,
         null=False,
     )
-    message = models.TextField(
-        verbose_name=_("Message"), help_text="Enter your message", max_length=1000
+
+    class Meta:
+        abstract = True
+
+
+class Contact(BasicContactInformation):
+    city = models.CharField(
+        verbose_name=_("City / Town"),
+        max_length=100,
+        null=False,
+        blank=False,
     )
+    message = models.TextField(verbose_name=_("Message"), max_length=1000)
     date_and_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -40,21 +41,7 @@ class Contact(models.Model):
         verbose_name_plural = "Contact Messages"
 
 
-class Newsletter(models.Model):
-    full_name = models.CharField(
-        verbose_name=_("Full Name"),
-        max_length=50,
-        help_text="Enter your full name",
-        null=False,
-        blank=False,
-    )
-    email = models.EmailField(
-        verbose_name=_("Email Address"),
-        help_text="Enter your email address",
-        max_length=300,
-        blank=False,
-        null=False,
-    )
+class Newsletter(BasicContactInformation):
     signup_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
