a789939c25562ade3a8363f754ccd3cf8e29fe4a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-02 19:37:44 +0000

    Add JS logic to handle showing and hiding the popup

--- static/js/custom/apartments-detail.js
+++ static/js/custom/apartments-detail.js
@@ -37,11 +37,26 @@ function displayImg(image_no) {
 /* Ask Question and Schedule meeting popups */
 const ask_question_popup = document.querySelector("#ask-question-popup");
 const ask_question_button = document.querySelector('.ask-question-btn');
-const question_form_close_button = document.querySelector('#question-form-close-btn')
+const question_form_close_button = document.querySelector('#question-form-close-btn');
 
-function handleAskButtonClick() {
+const schedule_meeting_popup = document.querySelector('#schedule-meeting-popup');
+const schedule_meeting_buttons = document.querySelectorAll('.schedule-meeting-btn');
+const schedule_meeting_form_close_button = document.querySelector('#schedule-meeting-form-close-btn')
+
+
+function handleAskQuestionButtonClick() {
   ask_question_popup.classList.toggle('hidden')
 }
 
-ask_question_button.onclick = handleAskButtonClick
-question_form_close_button.onclick = handleAskButtonClick
+function handleScheduleMeetingButtonClick() {
+  schedule_meeting_popup.classList.toggle('hidden')
+}
+
+ask_question_button.onclick = handleAskQuestionButtonClick
+question_form_close_button.onclick = handleAskQuestionButtonClick
+
+schedule_meeting_form_close_button.onclick = handleScheduleMeetingButtonClick
+
+for (const button of schedule_meeting_buttons) {
+  button.onclick = handleScheduleMeetingButtonClick
+}
