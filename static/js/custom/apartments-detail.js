688c768b1f4ba1c7917ccb4de2b3f3c82bea701f
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-02 19:09:14 +0000

    Rename the file and add js logic to handle displaying and hiding the ask question popup form

--- static/js/custom/apartments-detail.js
+++ static/js/custom/apartments-detail.js
@@ -32,3 +32,16 @@ function displayImg(image_no) {
   images[current_img - 1].style.display = 'block'
   dots[current_img - 1].className += ' dot-active'
 }
+
+
+/* Ask Question and Schedule meeting popups */
+const ask_question_popup = document.querySelector("#ask-question-popup");
+const ask_question_button = document.querySelector('.ask-question-btn');
+const question_form_close_button = document.querySelector('#question-form-close-btn')
+
+function handleAskButtonClick() {
+  ask_question_popup.classList.toggle('hidden')
+}
+
+ask_question_button.onclick = handleAskButtonClick
+question_form_close_button.onclick = handleAskButtonClick
