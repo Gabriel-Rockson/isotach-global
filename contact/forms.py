45c497c9ff4421c44025953f1ccbb181c433ac30
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-04 05:31:45 +0000

    Add the Schedule Meeting model form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contact, Newsletter, Inquiry
+from .models import Contact, Newsletter, Inquiry, ScheduleMeeting
 
 
 class ContactForm(forms.ModelForm):
@@ -20,4 +20,11 @@ class InquiryForm(forms.ModelForm):
     
     class Meta:
         model = Inquiry
+        fields = "__all__"
+        
+
+class ScheduleMeetingForm(forms.ModelForm):
+    
+    class Meta:
+        model = ScheduleMeeting
         fields = "__all__"
